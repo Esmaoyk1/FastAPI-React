6d8199a6a580cb1bc30965624baae744d7a795ef
Esmaoyk1 <94963496+Esmaoyk1@users.noreply.github.com>
2024-08-23 16:40:08 +0300

    Update database.py

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -7,4 +7,4 @@ engine = create_engine(URL_DATABASE , connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocokmmit = False  , autoflush = False , bind = engine)
 
 Base = declarative_base()
-
+# ** SQLite bağlandi.
